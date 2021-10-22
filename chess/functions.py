bb570a1b51e2c10486858b293ea7ed242d61143d
daniel-mooney <daniel.mooney180@gmail.com>
2021-10-22 13:55:32 +1100

    Completed conversion functions

--- chess/functions.py
+++ chess/functions.py
@@ -52,5 +52,15 @@ def grid_coord_to_num_coord(coord: str):
 
     return (row * 10) + column
 
+def grid_coord_to_index(coord: str):
+
+    row_index = [8 ,7, 6, 5, 4, 3, 2, 1]
+    
+    row = row_index[int(coord[1]) - 1]
+    column = ord(coord[0]) - 64
+
+    return (row, column)
+
+
 
 
