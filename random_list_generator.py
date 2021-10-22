ceb9110d87791704ffe9e66ead1f5c671cc3c531
daniel-mooney <daniel.mooney180@gmail.com>
2021-10-22 23:13:52 +1100

    created random_list_generator

---
+++ random_list_generator.py
@@ -0,0 +1,11 @@
+import random
+
+def generate_list(a, b, length, rounding = 2): 
+
+    if type(a) == int and type(b) == int:
+        return [random.randint(a, b) for i in range(length)]
+    elif type(a) == float or type(b) == float:
+        return [round(random.uniform(a, b), rounding) for i in range(length)]
+
+
+print(generate_list(0, 25, 15))
