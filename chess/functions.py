263a38295208ef3444807b65f21b36a29b321e5f
daniel-mooney <daniel.mooney180@gmail.com>
2021-10-28 14:00:19 +1100

    fixed num_coord index bug

--- chess/functions.py
+++ chess/functions.py
@@ -110,7 +110,7 @@ def num_coord_to_index(num_coord: int):
     
     row_index = [8 ,7, 6, 5, 4, 3, 2, 1]
 
-    row = row_index[num_coord // 10]
+    row = row_index[(num_coord // 10) - 1]
     column = num_coord % 10
 
     return (row, column)
--- test.py
+++ test.py
@@ -1,4 +1,10 @@
+def num_coord_to_index(num_coord: int):
+    
+    row_index = [8 ,7, 6, 5, 4, 3, 2, 1]
 
-a = '0'
+    row = row_index[num_coord // 10]
+    column = num_coord % 10
 
-print(a.isupper())
+    return (row, column)
+
+print(num_coord_to_index(84))
