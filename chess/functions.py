7516ffff14f0e4d2a87dfbcf4d38f81db6f86c2b
daniel-mooney <daniel.mooney180@gmail.com>
2021-10-30 21:54:14 +1100

    created control guide

--- chess/functions.py
+++ chess/functions.py
@@ -16,6 +16,32 @@ class Chess():
             ['2', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
             ['1', 'r', 'n', 'b', 'k', 'q', 'b', 'n', 'r']
         ])
+    
+    def reset_board(self):
+        self.board = np.array([
+            ["-", "A", "B", "C", "D", "E", "F", "G", "H"],
+            ['8', 'R', 'N', 'B', 'K', 'Q', 'B', 'N', 'R'],
+            ['7', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+            ['6', '0', '0', '0', '0', '0', '0', '0', '0'],
+            ['5', '0', '0', '0', '0', '0', '0', '0', '0'],
+            ['4', '0', '0', '0', '0', '0', '0', '0', '0'],
+            ['3', '0', '0', '0', '0', '0', '0', '0', '0'],
+            ['2', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+            ['1', 'r', 'n', 'b', 'k', 'q', 'b', 'n', 'r']
+        ])
+
+        print("The board has been reset")
+    
+    def print_control_guide(self):
+        key = 'Key: R - Rook, N - Knight, B - Bishop, K - King, Q - Queen, P - Pawn.'
+
+        print('\nLowercase is player 1, uppercase is player 2, player 1 goes first.')
+        print(key, end = '\n\n')
+        print(bcolours.BOLD + bcolours.GREEN + "Control Guide" + bcolours.ENDC)
+        print("-------------")
+        print("Choose the piece you wish to move when prompted, make sure the case type matches your player type.")
+        print("Enter the square you wish to move that piece to")
+
 
     def move_piece(self, current_cord: str, move_coord: str, player_number: int):
         
@@ -40,27 +66,9 @@ class Chess():
 
         return True
 
-    def reset_board(self):
-        self.board = np.array([
-            ["-", "A", "B", "C", "D", "E", "F", "G", "H"],
-            ['8', 'R', 'N', 'B', 'K', 'Q', 'B', 'N', 'R'],
-            ['7', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
-            ['6', '0', '0', '0', '0', '0', '0', '0', '0'],
-            ['5', '0', '0', '0', '0', '0', '0', '0', '0'],
-            ['4', '0', '0', '0', '0', '0', '0', '0', '0'],
-            ['3', '0', '0', '0', '0', '0', '0', '0', '0'],
-            ['2', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
-            ['1', 'r', 'n', 'b', 'k', 'q', 'b', 'n', 'r']
-        ])
-
-        print("The board has been reset")
-
     def play(self):
         # Message to terminal
-        key = 'Key: R - Rook, N - Knight, B - Bishop, K - King, Q - Queen, P - Pawn.'
-
-        print('\nLowercase is player 1, uppercase is player 2, player 1 goes first.')
-        print(key)
+        self.print_control_guide()
 
         player_number = 1
 
