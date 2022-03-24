a80fc83bafff173c4749cf597d17014ff28542e0
daniel-mooney <daniel.mooney180@gmail.com>
2022-03-24 19:14:11 +1100

    completed add_node

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -8,13 +8,13 @@ class Node:
         self.value = value
 
         # Next Node Pointers - left is less than, right is greater than.
-        self.left = None
-        self.right = None
+        self.left: Node = None
+        self.right: Node = None
 
 
 class BinaryTree:
     def __init__(self, node: Node) -> None:
-        self.base = node
+        self.base_node = node
     
     def __balancetree__(self) -> None:
         pass
@@ -26,7 +26,23 @@ class BinaryTree:
         Nodes are not allowed to have the same key of an existing node. This will result in an
         unsuccessful add_node call.
         """
+        if not isinstance(node, Node):
+            return False
 
+        current_node = self.base_node
+
+        while True:
+            # Loop until node is inserted or invalid
+            if node.key == current_node.key:
+                return False
+            if node.key > current_node.key and not current_node.right:
+                current_node.right = node
+                break
+            if node.key < current_node.key and not current_node.left:
+                current_node.left == node
+                break
+
+            current_node = current_node.right if node.key > current_node.key else current_node.left            
 
         return True
     
